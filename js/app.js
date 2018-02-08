63ee6a689022d6b2e3f2cc99f5973c73a4d19371
JackelineGCP <gessenia_17@hotmail.com>
2018-02-07 21:52:51 -0500

    agregando variables en js

--- js/app.js
+++ js/app.js
@@ -1,17 +1,56 @@
+// Declaramos las variables
+
+let btnPrimary= document.getElementById('btn-primary');
+let origen = document.getElementById('origen');
+let destino = document.getElementById('destino');
+let btnSecond = document.getElementById('btn-second');
+let maps = document.getElementById('map');
+
+
 function initMap() {
+  let directionsService = new google.maps.DirectionsService;
+  let directionsDisplay = new google.maps.DirectionsRenderer;
+
   let laboratoriaLima = {
     lat: -12.1191427,
-    log: -770349046
+    log: -77.0349046
   };
 
-  let map = new google.mpas.Map(document.getElementById('map'), {
+  let map = new google.maps.Map(maps, {
     zoom: 3,
     center: laboratoriaLima
   });
 
-  let markadorLaboratoria = new SVGFEMorphologyElement.maps.Marker({
+  let markadorLaboratoria = new google.maps.Marker({
     position: labaratoriaLima,
-    map: map
+    map: map,
+    animation: google.maps.Animation.DROP,
 
   });
 }
+
+function buscar(){
+  if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(functionExito, functionError);
+  }
+}
+
+let latitud, longitud;
+const functionExito = function (posicion){
+  latitud = posicion.coords.latitude;
+  longitud = posicion.coords.longitud;
+}
+
+let miUbicacion = new google.map.Marker({
+  position: {lat: latitud, lng: longitud},
+  map: map
+});
+
+map.setZoom(18);
+map.setCenter({lat: latitud, lng: longitud});
+
+let functionError = function (error){
+  error('Tenemos problemas con encontrar tu ubicación');
+}
+
+document.getElementById("encuentrame").addEventListener("click", buscar);
